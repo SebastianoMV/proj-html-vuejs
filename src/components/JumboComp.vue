43c44929303751e854ce59dceec8382ecfbb0a40
SebastianoMV <sebastianovisco@gmail.com>
2022-05-27 12:48:29 +0200

    preparazione layout fino rimo banner start

--- src/components/JumboComp.vue
+++ src/components/JumboComp.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="debug">
+    <div class="container ">
+      <div class="text-jumbo debug d-flex flex-column justify-content-center pt-5">
+        
+          <h1>Drive With Avada</h1>
+          <h3>We offer the finest Driving tuition money can buy</h3>
+        
+      </div>
+      
+    </div>
+
+    <div class="offert-up">
+      <div>
+
+      </div>
+      <div>
+
+      </div>
+    </div>
 
   </div>
 </template>
@@ -15,6 +33,26 @@ export default {
 
 div{
   height: 400px;
+  position: relative;
+  .container{
+    height: 100%;
+    .text-jumbo{
+      width: 50%;
+    }
+  }
+  .offert-up{
+    position: absolute;
+    top: 100px;
+    right: 10px;
+    div{
+      height: 50px;
+      width: 50px;
+      background-color: white;
+      z-index: 999;
+      border-radius: 10px;
+      margin: 5px;
+    }
+  }
 }
 
 </style>
--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,37 +1,59 @@
 <template>
-  <main class="debug">
-    <div class="debug container">
-      Get Behind The Wheel
+  <main class="">
+    <!-- BEHIND THE WEELS -->
+    
+    <div class="">
+      <div class="debug container d-flex justify-content-between">
+        <div class="text-behind debug">
+          Get Behind The Wheel
+        </div>
+        <div class="smv-w-50 debug margin-top-less-20">
+          first-lesson
+        </div>
+      </div>
     </div>
 
     <!-- BANNER START -->
-    <div class="debug container">
-      START
+    <div class="">
+      <div class="debug container d-flex justify-content-between align-items-center ">
+        <div>Start Learning</div>
+        <div>1234</div>
+      </div>
     </div>
 
     <!-- COURSES -->
-    <div class="debug container">
-      COURSES
+    <div class="debug">
+      <div class="debug container">
+        COURSES
+      </div>
     </div>
 
     <!-- INSTRUCTORS -->
-    <div class="debug container">
-      INSTRUCTORS
+    <div class="debug">
+      <div class="debug container">
+        INSTRUCTORS
+      </div>
     </div>
 
     <!-- TESTIMONIALS -->
-    <div class="debug container">
-      TESTIMONIALS
+    <div class="debug">
+      <div class="debug container">
+        TESTIMONIALS
+      </div>
     </div>
 
     <!-- BANNER NEWS -->
-    <div class="debug container">
-      NEWS
+    <div class="debug">
+      <div class="debug container">
+        NEWS
+      </div>
     </div>
 
      <!-- NEWSLETTERS -->
-    <div class="debug container">
-      NEWSLETTERS
+    <div class="debug">
+      <div class="debug container">
+        NEWSLETTERS
+      </div>
     </div>
   </main>
 </template>
@@ -46,6 +68,9 @@ export default {
 
 main{
   height: 100%;
+  .text-behind{
+    width: 45%;
+  }
 }
 
 </style>
