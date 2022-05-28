b3caeb71fdc982c9f90f672183419c2edd5f6c44
SebastianoMV <sebastianovisco@gmail.com>
2022-05-28 08:46:23 +0200

    impostazioni background per chiarezza grafica sections & inizio impostazioni grafiche

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,18 +1,22 @@
 <template>
   <footer>
     <!-- FOOTER -->
-    <div class="debug container">
-      <div class="row">
-        <div class="col debug"></div>
-        <div class="col debug"></div>
-        <div class="col debug"></div>
-        <div class="col debug"></div>
+    <div class="footer-bg background-cover">
+      <div class="debug container">
+        <div class="row">
+          <div class="col debug"></div>
+          <div class="col debug"></div>
+          <div class="col debug"></div>
+          <div class="col debug"></div>
+        </div>
       </div>
     </div>
 
      <!-- BANNER COPY -->
-    <div class="debug container">
-      BANNER COPY
+    <div class="copy-bg background-cover">
+      <div class="debug container">
+        BANNER COPY
+      </div>
     </div>
   </footer>
   
@@ -25,6 +29,13 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  .footer-bg{
+    background-image: url(../assets/img/footer-background-color.jpg);
+  }
+
+  .copy-bg{
+    background-color: black;
+  }
 
 </style>
--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -3,8 +3,8 @@
     <!-- BEHIND THE WEELS -->
     
     <div class="">
-      <div class="debug container d-flex justify-content-between">
-        <div class="text-behind debug">
+      <div class="container d-flex justify-content-between">
+        <div class="text-behind">
           <h2>Get Behind The Wheel</h2>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt id placeat magnam accusamus architecto necessitatibus at inventore hic illum. Omnis dolores hic odit provident iste nihil nisi eos accusamus unde.</p>
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla nobis qui quo! Ducimus, temporibus maxime. A nostrum minus in, blanditiis itaque eius repellendus quibusdam odit minima perferendis vitae eaque natus.</p>
@@ -28,7 +28,7 @@
     </div>
 
     <!-- COURSES -->
-    <div class="courses">
+    <div class="courses background-cover">
       <div class="container d-flex">
         <div class="courses-news margin-top-less-20">
 
@@ -44,7 +44,7 @@
     </div>
 
     <!-- % -->
-    <div class="">
+    <div class="rate-bg">
       <div class="debug container">
         <div class="row">
           <div class="col debug"></div>
@@ -77,26 +77,24 @@
     </div>
 
     <!-- TESTIMONIALS -->
-    <div class="debug">
-      <div class="debug container text-center">
+    <div class="debug testimonials-bg background-cover">
+      <div class=" container text-center">
         TESTIMONIALS
       </div>
     </div>
 
     <!-- BANNER NEWS -->
-    <div class="debug">
-      <div class="debug container d-flex flex-column align-items-center margin-top-less-20">
-        <div>
-          testo
+    <div class="debug banner-news-bg background-cover">
+      <div class=" container d-flex flex-column align-items-center margin-top-less-20">
+        <div class="news-container">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo accusantium quasi aliquam vero consectetur labore, aliquid minima ratione. Eum pariatur soluta nihil animi saepe maxime atque ad eos eius perferendis!
         </div>
-        <div>
-          freccia
-        </div>
+          <img src="../assets/img/latestnews-bottom.png" alt="">
       </div>
     </div>
 
      <!-- NEWSLETTERS -->
-    <div class="debug">
+    <div class="newsletter-bg background-cover">
       <div class="debug container d-flex flex-column align-items-center" >
         <h3>Sing up to our Newsletters</h3>
         <input type="text">
@@ -132,7 +130,7 @@ main{
   //COURSES
   .courses{
     background-image: url(../assets/img/promise-background.jpg);
-    background-position: bottom left;
+    background-position: center;
     background-size: cover;
 
     .courses-news{
@@ -142,6 +140,40 @@ main{
       margin-bottom: 30px;
     }
 
+  }
+
+  //rate
+  .rate-bg{
+    background-image: url(../assets/img/latestnews-bottom.png);
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-color: blue;
+  }
+
+  // testimonials
+  .testimonials-bg{
+    background-image: url(../assets/img/testimonial-background.jpg);
+    height: 400px; //rimuovere
+  }
+
+  // banner news
+  .banner-news-bg{
+    background-image: url(../assets/img/latestnews-background.jpg);
+    height: 400px; //rimuovere
+    .news-container{
+      width: 50%;
+      height: 100%;
+      background-color: white;
+      border-radius: 3px;
+    }
+    img{
+      width: 50%;
+    }
+  }
+
+  // newsletter
+  .newsletter-bg{
+    background-image: url(../assets/img/newsletter_background.jpg);
   }
 }
 
